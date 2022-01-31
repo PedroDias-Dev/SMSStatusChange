85bd0eb88077720fd34a7120de237f03ce3fe41f
Pedro Dias <pedro.dias@comuniki.me>
2022-01-30 21:29:44 -0300

    Projeto iniciado

---
+++ ChangeStatusJob.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Extensions.Configuration;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace SMSStatusChange
+{
+    public class ChangeStatusJob : IJob
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public class Mensagem
+        {
+            public string status { get; set; }
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            SqlConnection sqlConn = new SqlConnection(System.Configuration.ConfigurationManager.AppSettings.Get("ConnectionString"));
+
+            sqlConn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM MS003 WHERE STAENVIO ='R' AND DATENVIO >='2021-12-15'", sqlConn);
+
+            //  tabela de código de status
+            SqlCommand cmdStatus = new SqlCommand("SELECT * FROM MS029", sqlConn);
+            //  tabela de tokens de autenticação
+            SqlCommand cmdToken = new SqlCommand("SELECT CODPWDCONFIG FROM SG028 WHERE IDCONFIGAPL = 50", sqlConn);
+            var apiToken = cmdToken.ExecuteScalar();
+
+            SqlDataReader drStatus = cmdStatus.ExecuteReader();
+
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            while (drStatus.Read())
+            {
+                try
+                {
+                    dict.Add(drStatus["DCRSTATUSENVIO"].ToString(), drStatus["CODSTATUSENVIO"].ToString());
+                }
+                catch (Exception ex)
+                {
+                    Console.Out.WriteLine(ex.Message);
+                }
+            }
+
+            drStatus.Close();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.Out.WriteLine(dr["IDSMS"] + " - " + dr["STAENVIO"]);
+
+                try
+                {
+                    //Console.WriteLine(apiToken.ToString());
+                    var responseString = await client.GetStringAsync("https://v2.bestuse.com.br/api/v1/envioApi/getStatus?token=" + apiToken.ToString() + "&id=" + dr["IDSMS"]);
+
+                    var jsonResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<Mensagem>(responseString);
+                    Console.Out.WriteLine(jsonResponse.status);
+
+                    //SqlCommand updateCodeCmd = new SqlCommand();
+
+                    if (jsonResponse.status == "NAO ENTREGUE")
+                    {
+                        //updateCodeCmd = new SqlCommand("UPDATE MS003 SET CODSTATUSENVIO = " + dict["NÃO ENTREGUE"] + " WHERE Id = " + dr["IDSMS"], sqlConn);
+                        Console.Out.WriteLine(dict["NÃO ENTREGUE"] + " " + DateTime.Now);
+                    }
+                    else if (jsonResponse.status == null)
+                    {
+                        Console.Out.WriteLine("NULL" + " " + DateTime.Now);
+                        throw new Exception(jsonResponse.status);
+                    }
+                    else
+                    {
+                        //updateCodeCmd = new SqlCommand("UPDATE MS003 SET CODSTATUSENVIO = " + dict[jsonResponse.status] + " WHERE Id = " + dr["IDSMS"], sqlConn);
+                        Console.Out.WriteLine(dict[jsonResponse.status] + " " + DateTime.Now);
+                    }
+
+                    //updateCodeCmd.BeginExecuteNonQuery();
+
+                    // alterar campo de tentativas ===> UPDATE MS003 SET QTDTENTATIVA = QTDTENTATIVA + 1 WHERE Id = + dr["IDSMS"]
+                    //SqlCommand updateQtyCmd = new SqlCommand("UPDATE MS003 SET QTDTENTATIVA = QTDTENTATIVA + 1 WHERE Id = " + dr["IDSMS"], sqlConn);
+                    //updateQtyCmd.BeginExecuteNonQuery();
+
+                    // alterar campo de data verificação
+                    //SqlCommand updateDateCmd = new SqlCommand("UPDATE MS003 SET DATVERIFICAO = " + DateTime.Now + " WHERE Id = " + dr["IDSMS"], sqlConn);
+                    //updateDateCmd.BeginExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.Out.WriteLine(ex.ToString());
+                }
+
+            }
+
+            sqlConn.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Logging;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Http;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace SMSStatusChange
+{
+    public class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            LogProvider.SetCurrentLogProvider(new ConsoleLogProvider());
+
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+            IScheduler scheduler = await factory.GetScheduler();
+
+            await scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<ChangeStatusJob>()
+                .WithIdentity("job1", "group1")
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(10000)
+                    //.WithIntervalInMinutes(30)
+                    .RepeatForever())
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+
+            //await Task.Delay(TimeSpan.FromSeconds(10));
+
+            //await scheduler.Shutdown();
+
+            Console.ReadKey();
+        }
+    }
+}
